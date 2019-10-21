df99864339c52fc77c7831d5c34e967f9ad4e332
Shalu411687 <55138128+Shalu411687@users.noreply.github.com>
2019-10-21 16:30:17 +0530

    shalu
    shalu added

---
+++ great-outdoors/src/main/java/com/capgemini/go/entity/WishlistEntity.java
@@ -0,0 +1,42 @@
+package com.capgemini.go.entity;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_wishlist")
+public class WishlistEntity {
+	// attributes
+	@Column(name = "userId", unique = true, nullable = false)
+	private String userId;
+	
+	@Column(name = "productId", unique = true, nullable = false)
+	private String productId;
+	
+	// getters
+	public String getUserId () {return this.userId;}
+	public String getProductId () {return this.productId;}
+	
+	// setters
+	void setUserId (String userId) {this.userId = userId;}
+	void setProductId (String productId) {this.productId = productId;}
+	
+	// constructors
+	public WishlistEntity () {
+		
+	}
+	
+	public WishlistEntity (String userId, String productId )
+	{
+		this.userId = userId;
+		this.productId = productId;
+	}
+	
+
+        String result = "[retailerId: " + this.userId + ", productId: " + this.productId + "]";
+    }
+
